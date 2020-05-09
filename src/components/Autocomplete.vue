c34d475a7337d96848c9b172df0818998221177a
Alessandro Catania <alessandro.catania@concirrus.com>
2020-05-09 01:09:32 +0100

    Buit distribution package

--- src/components/Autocomplete.vue
+++ src/components/Autocomplete.vue
@@ -19,24 +19,33 @@
           </v-chip>
         </div>
       </draggable>
-       <draggable
+      <draggable
         class="dragArea list-group"
         :list="values"
-        :group="{ name: 'people', pull: false, put: true }" >
-      <div class="input-Box ma-1">
-        <input type="text" class="input-ids" v-model="value" @keyup.down="currentDropdownSelection++" @keyup.up="selectNext" @input="currentDropdownSelection = 0" @keyup.enter="enterValueFromAutocomplete"/>
-      </div>
-       </draggable>
+        :group="{ name: 'people', pull: false, put: true }"
+      >
+        <div class="input-Box ma-1">
+          <input
+            type="text"
+            class="input-ids"
+            v-model="value"
+            @keyup.down="currentDropdownSelection++"
+            @keyup.up="selectNext"
+            @input="currentDropdownSelection = 0"
+            @keyup.enter="enterValueFromAutocomplete"
+          />
+        </div>
+      </draggable>
     </div>
     <div class="autocomplete" v-if="autocomplete.length > 0">
       <div
         v-for="(item, index) in autocomplete"
         :key="index"
-        :class="{active: index == currentDropdownSelection, result: true}"
+        :class="{ active: index == currentDropdownSelection, result: true }"
         @click="addFromAutocomplete(item)"
       >
         {{ item.label }}
-        <span v-for="(ip, index2) in item.ips" :key="index2" >{{ ip }} / </span>
+        <span v-for="(ip, index2) in item.ips" :key="index2">{{ ip }} / </span>
       </div>
     </div>
     <pre>{{ formattedJson }}</pre>
@@ -70,12 +79,12 @@ export default {
   },
   methods: {
     addFromAutocomplete(item) {
-      if (this.checkIfAlreadyExist(this.values,item.id))
-      this.values.push({
-        id: item.id,
-        label: item.label,
-        ips: item.ips
-      });
+      if (this.checkIfAlreadyExist(this.values, item.id))
+        this.values.push({
+          id: item.id,
+          label: item.label,
+          ips: item.ips
+        });
       this.value = "";
     },
     remove(id) {
@@ -85,21 +94,26 @@ export default {
     },
     checkIfAlreadyExist(arrayofValues, id) {
       let alreadyExist = true;
-     arrayofValues.map(item => {        
-        if( item.id === id) alreadyExist = false;
+      arrayofValues.map(item => {
+        if (item.id === id) alreadyExist = false;
       });
-    return alreadyExist;
+      return alreadyExist;
     },
-    checkMove: function(evt){
-      return this.checkIfAlreadyExist(evt.relatedContext.list,evt.draggedContext.element.id);
+    checkMove: function(evt) {
+      return this.checkIfAlreadyExist(
+        evt.relatedContext.list,
+        evt.draggedContext.element.id
+      );
     },
     selectNext() {
-      console.log('select');
+      console.log("select");
       this.currentDropdownSelection--;
     },
     enterValueFromAutocomplete() {
       if (this.autocomplete.length !== 0)
-      this.addFromAutocomplete(this.autocomplete[this.currentDropdownSelection])
+        this.addFromAutocomplete(
+          this.autocomplete[this.currentDropdownSelection]
+        );
     }
   },
   computed: {
@@ -109,7 +123,7 @@ export default {
         ip => ip.label.toLowerCase().indexOf(this.value.toLowerCase()) >= 0
       );
     },
-    formattedJson(){
+    formattedJson() {
       return JSON.stringify(this.values, null, 2);
     }
   }
@@ -121,30 +135,33 @@ export default {
   width: 50%;
 }
 .dragArea.list-group {
-// flex: 0; 
-display: flex;
-flex-wrap: wrap;
+  // flex: 0;
+  display: flex;
+  flex-wrap: wrap;
 }
 .input {
   border: 2px solid #000;
   display: flex;
   flex-wrap: wrap;
-      max-width: 100%;
+  max-width: 100%;
   .list-group:last-child {
     // flex-basis: 100%;
     // flex-shrink: 0;
     // flex-grow: 1;
     flex: 1;
-}
+  }
 }
 
 input {
   width: 100%;
 }
-input:focus, textarea { outline: none ! important; }
+input:focus,
+textarea {
+  outline: none !important;
+}
 .input-Box {
   // border: 1px solid black;
-  width: 100%
+  width: 100%;
 }
 .input-ids {
   height: 100%;
@@ -156,7 +173,8 @@ input:focus, textarea { outline: none ! important; }
   .result {
     width: 100%;
     border-bottom: 1px solid gray;
-    &:hover, &.active {
+    &:hover,
+    &.active {
       background: #f4f3f3;
       cursor: pointer;
     }
